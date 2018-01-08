931e946b81f40ad801a2a8d78bcfb26becf3789d
aleksey.kuchik <aleksey.kuchik@itechart-group.com>
2018-01-08 15:29:05 +0300

    implemented service method call

--- api/controllers/sample.js
+++ api/controllers/sample.js
@@ -1,15 +1,16 @@
 const BaseController = require('./base');
 
 class SampleController extends BaseController {
-    constructor({ sampleService, logger }) {
-        super(sampleService, logger);
-    }
+  constructor({ sampleService, logger }) {
+    super(sampleService, logger);
+  }
 
-    async sampleRequest(req, res) {
-        const params = await this._validateParams(req.query, ['a']);
-        this.logger.info(params);
-        res.status(201).json(params);
-    }
+  async sampleRequest(req, res) {
+    const params = await this._validateParams(req.query, ['a']);
+    const result = await this._service.sampleRequest(params);
+    this.logger.info(result);
+    res.status(201).json(result);
+  }
 }
 
 module.exports = SampleController;
--- services/sample.js
+++ services/sample.js
@@ -1,9 +1,14 @@
 const BaseService = require('./base');
 
 class SampleService extends BaseService {
-    constructor({ logger }) {
-        super();
-    }
+  constructor({ logger }) {
+    super();
+    this._logger = logger;
+  }
+  sampleRequest(params) {
+    this._logger.info('service method has been called');
+    return params;
+  }
 }
 
 module.exports = SampleService;
